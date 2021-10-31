2bf974f93937a1f23d9796555ba0d99b573aa412
hphp777 <85219925+hphp777@users.noreply.github.com>
2021-10-31 11:02:40 +0900

    table5

--- create_db.php
+++ create_db.php
@@ -13,11 +13,9 @@ if ($conn->connect_error) {
  
 // create table0
 $sql = "CREATE TABLE Athletes (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-aname VARCHAR(30) NOT NULL,
-country VARCHAR(30) NOT NULL,
-age INT(2) NOT NULL,
-totalMedalNum INT(2) NOT NULL
+AName VARCHAR(30) NOT NULL,
+Country VARCHAR(30) NOT NULL,
+Discipline VARCHAR(30) NOT NULL
 )";
  
 if ($conn->query($sql) === TRUE) {
@@ -27,12 +25,11 @@ if ($conn->query($sql) === TRUE) {
 }
 
 // create table1
-$sql = "CREATE TABLE Country (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    cname VARCHAR(30) NOT NULL,
-    numOfAthletes INT(4) NOT NULL,
-    totalMedalNum INT(2) NOT NULL,
-    totalWinningNum INT(3) NOT NULL,
+$sql = "CREATE TABLE Coaches (
+    CName VARCHAR(30) NOT NULL,
+    Country VARCHAR(30) NOT NULL,
+    Discipline VARCHAR(30) NOT NULL,
+    Discription VARCHAR(30) NOT NULL,
     )";
 
 if ($conn->query($sql) === TRUE) {
@@ -42,9 +39,13 @@ if ($conn->query($sql) === TRUE) {
 }
 // create table2
 $sql = "CREATE TABLE Medal (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    mname VARCHAR(30) NOT NULL,
-    color VARCHAR(25) NOT NULL,
+    Ranking INT(6) NOT NULL, 
+    Country VARCHAR(30) NOT NULL,
+    Gold INT(6) NOT NULL,
+    Silver INT(6) NOT NULL,
+    Bronze INT(6) NOT NULL,
+    Total INT(6) NOT NULL,
+    RankByTotal INT(6) NOT NULL
     )";
 
 if ($conn->query($sql) === TRUE) {
@@ -54,9 +55,31 @@ if ($conn->query($sql) === TRUE) {
 }
 
 // create table3
+$sql = "CREATE TABLE EntriesGender (
+    Discipline VARCHAR(30) NOT NULL, 
+    Female INT(6) NOT NULL,
+    Male INT(6) NOT NULL,
+    Total INT(6) NOT NULL,
+    )";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 // create table4
+$sql = "CREATE TABLE Teams (
+    Country VARCHAR(30) NOT NULL, 
+    Discipline VARCHAR(30) NOT NULL,
+    NOC VARCHAR(30) NOT NULL, 
+    Gender VARCHAR(30) NOT NULL, 
+    )";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 // create table5
 
 // create table6
